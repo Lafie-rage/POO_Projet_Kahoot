449cd44b761a7e5ae90ae4992a01d6a0ec4357c4
elfamososam <49751660+elfamososam@users.noreply.github.com>
2020-12-05 18:16:05 +0100

    extend capacity of question + put unique on category

--- kahoot (1).sql
+++ kahoot (1).sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 05 déc. 2020 à 12:17
--- Version du serveur :  8.0.21
+-- Généré le : sam. 05 déc. 2020 à 17:13
+-- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `answer`;
 CREATE TABLE IF NOT EXISTS `answer` (
-  `ID_answer` int NOT NULL AUTO_INCREMENT,
-  `TEXTE_answer` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `ID_answer` int(11) NOT NULL AUTO_INCREMENT,
+  `TEXTE_answer` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`ID_answer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -42,10 +42,11 @@ CREATE TABLE IF NOT EXISTS `answer` (
 
 DROP TABLE IF EXISTS `category`;
 CREATE TABLE IF NOT EXISTS `category` (
-  `ID_Category` int NOT NULL AUTO_INCREMENT,
-  `TEXTE_Category` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  PRIMARY KEY (`ID_Category`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ID_Category` int(11) NOT NULL AUTO_INCREMENT,
+  `TEXTE_Category` varchar(50) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`ID_Category`),
+  UNIQUE KEY `TEXTE_Category` (`TEXTE_Category`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,12 +56,12 @@ CREATE TABLE IF NOT EXISTS `category` (
 
 DROP TABLE IF EXISTS `game`;
 CREATE TABLE IF NOT EXISTS `game` (
-  `ID_Game` int NOT NULL AUTO_INCREMENT,
+  `ID_Game` int(11) NOT NULL AUTO_INCREMENT,
   `Date` date NOT NULL,
-  `id_category` int NOT NULL,
+  `id_category` int(11) NOT NULL,
   PRIMARY KEY (`ID_Game`),
   KEY `fk_category` (`id_category`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -70,11 +71,11 @@ CREATE TABLE IF NOT EXISTS `game` (
 
 DROP TABLE IF EXISTS `player`;
 CREATE TABLE IF NOT EXISTS `player` (
-  `ID_Player` int NOT NULL AUTO_INCREMENT,
-  `login` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `ID_Player` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `password` varchar(20) CHARACTER SET latin1 COLLATE latin1_general_ci NOT NULL,
   PRIMARY KEY (`ID_Player`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -84,11 +85,11 @@ CREATE TABLE IF NOT EXISTS `player` (
 
 DROP TABLE IF EXISTS `proposition`;
 CREATE TABLE IF NOT EXISTS `proposition` (
-  `answer_ID_answer` int NOT NULL,
-  `QUESTION_ID_QUESTION` int NOT NULL,
+  `answer_ID_answer` int(11) NOT NULL,
+  `QUESTION_ID_QUESTION` int(11) NOT NULL,
   PRIMARY KEY (`answer_ID_answer`,`QUESTION_ID_QUESTION`),
   KEY `fk_PROPOSITION_QUESTION1` (`QUESTION_ID_QUESTION`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -98,14 +99,14 @@ CREATE TABLE IF NOT EXISTS `proposition` (
 
 DROP TABLE IF EXISTS `question`;
 CREATE TABLE IF NOT EXISTS `question` (
-  `ID_QUESTION` int NOT NULL AUTO_INCREMENT,
-  `TEXTE_QUESTION` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `Category_ID_Category` int NOT NULL,
-  `ID_BONNE_answer` int NOT NULL,
+  `ID_QUESTION` int(11) NOT NULL AUTO_INCREMENT,
+  `TEXTE_QUESTION` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+  `Category_ID_Category` int(11) NOT NULL,
+  `ID_BONNE_answer` int(11) NOT NULL,
   PRIMARY KEY (`ID_QUESTION`,`Category_ID_Category`,`ID_BONNE_answer`),
   KEY `fk_QUESTION_Category1` (`Category_ID_Category`),
   KEY `fk_QUESTION_answer1` (`ID_BONNE_answer`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -115,11 +116,11 @@ CREATE TABLE IF NOT EXISTS `question` (
 
 DROP TABLE IF EXISTS `question_game`;
 CREATE TABLE IF NOT EXISTS `question_game` (
-  `Game_ID_Game` int NOT NULL,
-  `QUESTION_ID_QUESTION` int NOT NULL,
+  `Game_ID_Game` int(11) NOT NULL,
+  `QUESTION_ID_QUESTION` int(11) NOT NULL,
   PRIMARY KEY (`Game_ID_Game`,`QUESTION_ID_QUESTION`),
   KEY `fk_QUESTION_Game_QUESTION1` (`QUESTION_ID_QUESTION`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -129,12 +130,12 @@ CREATE TABLE IF NOT EXISTS `question_game` (
 
 DROP TABLE IF EXISTS `score`;
 CREATE TABLE IF NOT EXISTS `score` (
-  `Id_player` int NOT NULL,
-  `id_game` int NOT NULL,
-  `Score` int NOT NULL,
+  `Id_player` int(11) NOT NULL,
+  `id_game` int(11) NOT NULL,
+  `Score` int(11) NOT NULL,
   PRIMARY KEY (`Id_player`,`id_game`),
   KEY `Fk_game` (`id_game`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Contraintes pour les tables déchargées
@@ -150,15 +151,15 @@ ALTER TABLE `game`
 -- Contraintes pour la table `proposition`
 --
 ALTER TABLE `proposition`
-  ADD CONSTRAINT `fk_PROPOSITION_answer1` FOREIGN KEY (`answer_ID_answer`) REFERENCES `answer` (`ID_answer`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_PROPOSITION_QUESTION1` FOREIGN KEY (`QUESTION_ID_QUESTION`) REFERENCES `question` (`ID_QUESTION`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_PROPOSITION_QUESTION1` FOREIGN KEY (`QUESTION_ID_QUESTION`) REFERENCES `question` (`ID_QUESTION`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_PROPOSITION_answer1` FOREIGN KEY (`answer_ID_answer`) REFERENCES `answer` (`ID_answer`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `question`
 --
 ALTER TABLE `question`
-  ADD CONSTRAINT `fk_QUESTION_answer1` FOREIGN KEY (`ID_BONNE_answer`) REFERENCES `answer` (`ID_answer`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_QUESTION_Category1` FOREIGN KEY (`Category_ID_Category`) REFERENCES `category` (`ID_Category`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fk_QUESTION_Category1` FOREIGN KEY (`Category_ID_Category`) REFERENCES `category` (`ID_Category`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_QUESTION_answer1` FOREIGN KEY (`ID_BONNE_answer`) REFERENCES `answer` (`ID_answer`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Contraintes pour la table `question_game`
