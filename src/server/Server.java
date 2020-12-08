1b5af4a55d90e8de0f9a6533933740884fcf43bc
Lafie-rage <Lafie-rage@github.com>
2020-12-08 09:44:24 +0100

    Moving psvm to the bottom of the class

--- src/server/Server.java
+++ src/server/Server.java
@@ -2,76 +2,56 @@ package server;
 
 import java.io.*;
 
-
-import java.net.Socket;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import java.net.ServerSocket;
 
-public class Server extends Thread
-{
-    private Lobby lobby;
-
-    private static final int port=60000;
-    private ServerSocket serverSocket;
+public class Server extends Thread {
+    private static final int port = 60000;
     private static List<Connection> connectionList;
+    private Lobby lobby;
+    private ServerSocket serverSocket;
 
-    public Server() throws IOException
-    {
-        lobby =new Lobby();
+    public Server() throws IOException {
+        lobby = new Lobby();
         this.serverSocket = new ServerSocket(port);
         connectionList = new ArrayList<>();
     }
 
-    private void closeListeningSocket()
-    {
-        try
-        {
+    public static synchronized List<Connection> getListConnexion() {
+        return connectionList;
+    }
+
+    private void closeListeningSocket() {
+        try {
             serverSocket.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             System.err.println("Error while closing server socket");
             e.printStackTrace();
         }
     }
 
-    public static synchronized List<Connection> getListConnexion()
-    {
-        return connectionList;
-    }
-
     @Override
-    public void run()
-    {
-        try
-        {
-            while(true)
-            {
+    public void run() {
+        try {
+            while (true) {
                 Connection con = new Connection(serverSocket.accept());
                 System.out.println("co");
-                synchronized(connectionList)
-                {
+                synchronized (connectionList) {
                     connectionList.add(con);
                     con.start();
                     //lobby.addPlayerInLobby(con);
                 }
             }
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally
-        {
+        } finally {
             closeListeningSocket();
         }
     }
 
-    public void removeConnection()
-    {
+    public void removeConnection() {
 
     }
 
@@ -80,5 +60,4 @@ public class Server extends Thread
         Server serv = new Server();
         serv.start();
     }
-
 }
