adb5a6d3450e8bacd8887714b3d22303edd15cee
Lafie-rage <Lafie-rage@github.com>
2020-12-08 11:26:49 +0100

    Implementing a Commons class to store consts between classes

---
+++ src/utils/Commons.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * Class containing definition of commons final values shared in the whole project.
+ */
+public class Commons {
+    // region Network configuration
+    public static final String HOST = "127.0.0.1";
+    public static final int PORT = 60_000;
+    // endregion
+
+    // region Communication signals
+    public static final String ENDING_CONNECTION_SIGNAL = "ENDING_CONNECTION";
+    // endregion
+}
